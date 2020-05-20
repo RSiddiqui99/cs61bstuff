f2bef51cc68cb5081c5adce385547d2d1cbc6312
Raiyan <raiyan_siddiqui1999@hotmail.com>
2020-05-20 00:17:18 -0400

    second

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,28 @@ public class LeapYear {
             }
         }
     }
+    
+    public static boolean isLeapYear(int year)
+    {
+    	if (year%400==0)
+    	{
+    		return true;
+    	}
+    	
+    	else if (year%4==0 && year%100!=0)
+    	{
+    		return true;
+    	}
+    	
+    	return false;
+
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
