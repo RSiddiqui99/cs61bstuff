3aaa5da1be3bd6212ed0543a7d5ebc0c2dcb22eb
Raiyan <raiyan_siddiqui1999@hotmail.com>
2020-07-24 17:28:45 -0400

    update

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -3,8 +3,6 @@ public class UnionFind
 
     int numbers[];
 
-    /* Creates a UnionFind data structure holding n vertices. Initially, all
-       vertices are in disjoint sets. */
     public UnionFind(int n)
     {
         numbers=new int[n];
@@ -15,7 +13,6 @@ public class UnionFind
         }
     }
 
-    /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex)
     {
         if (vertex<0||vertex>numbers.length-1)
@@ -25,21 +22,22 @@ public class UnionFind
 
     }
 
-    /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1)
     {
+        validate(v1);
         return (numbers[find(v1)]*(-1));
     }
 
-    /* Returns the parent of v1. If v1 is the root of a tree, returns the
-       negative size of the tree for which v1 is the root. */
+
     public int parent(int v1)
     {
+        validate(v1);
         return numbers[v1];
     }
-
-    /* Returns true if nodes v1 and v2 are connected. */
-    public boolean connected(int v1, int v2) {
+    
+    public boolean connected(int v1, int v2)
+    {
+        validate(v1);
 
         if (find(v1)==find(v2))
         {
@@ -49,13 +47,17 @@ public class UnionFind
         return false;
     }
 
-    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
-       elements, and a union-by-size heuristic is used. If the sizes of the sets
-       are equal, tie break by connecting v1's root to v2's root. Unioning a 
-       vertex with itself or vertices that are already connected should not 
-       change the sets but may alter the internal structure of the data. */
+
     public void union(int v1, int v2)
     {
+        validate(v1);
+        validate(v2);
+
+        if (connected(v1,v2)==true)
+        {
+            return;
+        }
+
         if (numbers[find(v1)] == numbers[find(v2)])
         {
             numbers[find(v1)]=find(v2);
@@ -75,11 +77,11 @@ public class UnionFind
         }
     }
 
-    /* Returns the root of the set V belongs to. Path-compression is employed
-       allowing for fast search-time. */
 
     public int find(int vertex)
     {
+        validate(vertex);
+
         int root=vertex;
 
         while (numbers[root]>=0)
